7e5da7ac9233d5c51e70a9300d3104d8d4419a21
devang0220 <bhutedd@rknec.edu>
2024-08-07 21:29:00 +0530

    Add files via upload

---
+++ m.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class m {
+    public static void main(String[] args) {
+        System.out.println("BANKAI");
+        Scanner sc = new Scanner(System.in);
+        
+   String fruits=sc.nextLine();
+System.out.println(func(fruits));
+    }
+static boolean func(String fruits){
+    String s="s";
+    if(fruits==s)return true;
+    return false;
+}
+static void func(int ...v){
+    ArrayList<Integer> a=new ArrayList<>();
+    a.toArray
+}
+}
